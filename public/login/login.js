8b04afecab26cdf3aba3d80f2bcbf6f3f6bd9f84
Eduardo Morais <dudu.vgvm@gmail.com>
2024-01-03 15:54:39 -0300

    Aula2

--- public/login/login.js
+++ public/login/login.js
@@ -1,11 +1,11 @@
-import { emitirCadastrarUsuario } from "./socket-front-cadastro.js";
+import { emitirAutenticarUsuario } from "./socket-front-login.js";
 
-const form = document.getElementById("form-cadastro");
+const form = document.getElementById("form-login");
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
     const nome = form["input-usuario"].value;
     const senha = form["input-senha"].value;
 
-    emitirCadastrarUsuario({ nome, senha });
+    emitirAutenticarUsuario({ nome, senha });
 })
